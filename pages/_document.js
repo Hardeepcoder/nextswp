ae425c26b404bc82a0bb06dc7fa6576dea3db1b9
EasyCode Sardar <hardeepphp@yahoo.com>
2021-11-26 13:13:34 +0530

    updates

--- pages/_document.js
+++ pages/_document.js
@@ -15,12 +15,12 @@ class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
-          <script strategy="lazyOnload" src="vendor/jquery/jquery.min.js"></script>
-  <script  strategy="lazyOnload"  src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <script strategy="lazyOnload"  src="assets/js/owl-carousel.js"></script>
-  <script strategy="lazyOnload"  src="assets/js/animation.js"></script>
-  <script strategy="lazyOnload"  src="assets/js/imagesloaded.js"></script>
-  <script strategy="lazyOnload"  src="assets/js/custom.js"></script>
+          <script  src="vendor/jquery/jquery.min.js"></script>
+  <script    src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script   src="assets/js/owl-carousel.js"></script>
+  <script   src="assets/js/animation.js"></script>
+  <script   src="assets/js/imagesloaded.js"></script>
+  <script   src="assets/js/custom.js"></script>
   
      
         </body>
