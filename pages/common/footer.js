409cc26080e7dedabebc9bef20b78744887ad0dc
hardeep singh <hardeepsingh@192.168.1.6>
2021-11-26 07:53:40 +0530

    updates

--- pages/common/footer.js
+++ pages/common/footer.js
@@ -14,12 +14,7 @@ function Footer() {
         </div>
       </div>
     </footer>
-    <script src="vendor/jquery/jquery.min.js"></script>
-  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <script src="assets/js/owl-carousel.js"></script>
-  <script src="assets/js/animation.js"></script>
-  <script src="assets/js/imagesloaded.js"></script>
-  <script src="assets/js/custom.js"></script>
+    
         </>
     )
 }
